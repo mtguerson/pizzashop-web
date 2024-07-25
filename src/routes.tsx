c8af316b78e15d5e9d760dafc398ce79b3f72aa8
mtguerson <mtguerson@gmail.com>
2024-07-25 17:12:48 -0400

    dashboard ui

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app.tsx'
 import { AuthLayout } from './pages/_layouts/auth.tsx'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard.tsx'
 import { Orders } from './pages/app/orders/orders.tsx'
 import { SignIn } from './pages/auth/sign-in.tsx'
 import { SignUp } from './pages/auth/sign-up.tsx'
