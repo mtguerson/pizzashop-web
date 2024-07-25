ec6b0704919de694734f4dea6c674053b305bf92
mtguerson <mtguerson@gmail.com>
2024-07-25 12:09:55 -0400

    add toast (sonner)

--- src/pages/auth/sign-in.tsx.tsx
+++ src/pages/auth/sign-in.tsx.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,9 +21,16 @@ export function SignIn() {
   } = useForm<SignForm>()
 
   async function handleSignIn(data: SignForm) {
-    console.log(data)
-
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
