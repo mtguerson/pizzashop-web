2327fb07455db9c8f90b27d42760dc31eeff91f7
mtguerson <mtguerson@gmail.com>
2024-07-25 16:02:38 -0400

    add orders list

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app.tsx'
 import { AuthLayout } from './pages/_layouts/auth.tsx'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders.tsx'
 import { SignIn } from './pages/auth/sign-in.tsx'
 import { SignUp } from './pages/auth/sign-up.tsx'
 
@@ -10,7 +11,10 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
 
   {
