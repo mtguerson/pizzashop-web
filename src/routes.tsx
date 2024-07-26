1da5cae76cbeea37104d89d3ae2637b1244be2ab
mtguerson <mtguerson@gmail.com>
2024-07-26 12:58:37 -0400

    404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app.tsx'
 import { AuthLayout } from './pages/_layouts/auth.tsx'
+import { NotFound } from './pages/404.tsx'
 import { Dashboard } from './pages/app/dashboard/dashboard.tsx'
 import { Orders } from './pages/app/orders/orders.tsx'
 import { SignIn } from './pages/auth/sign-in.tsx'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
